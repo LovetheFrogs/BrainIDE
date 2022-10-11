41aa6ee7ae660b60cddef2f3d5367d1fe214d1ff
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-10-11 12:20:29 +0200

    Implemented colormap class

--- CompilerApp/Colormap.py
+++ CompilerApp/Colormap.py
@@ -1,12 +1,41 @@
+import pickle
+
+
 class Colormap:
-    def __init__(self):
-        raise NotImplementedError("To be implemented.")
+    def __init__(self, default):
+        if default:
+            self.colormap = {
+                ']': '#a94926',
+                '+': '#cc7832',
+                '-': '#cc7832',
+                '<': '#6a8759',
+                '>': '#6a8759',
+                ',': '#6396ba',
+                '.': '#6396ba',
+                '[': '#a94926'
+            }
+        else:
+            self.colormap = self.__load_colormap()
 
     def restore_default(self):
-        raise NotImplementedError("To be implemented.")
+        self.colormap = {
+                ']': '#a94926',
+                '+': '#cc7832',
+                '-': '#cc7832',
+                '<': '#6a8759',
+                '>': '#6a8759',
+                ',': '#6396ba',
+                '.': '#6396ba',
+                '[': '#a94926'
+            }
 
+    @staticmethod
     def __load_colormap(self):
-        raise NotImplementedError("To be implemented.")
+        with open(r'resources//data.pkl', 'rb') as file:
+            colormap = pickle.load(file)
+
+        return colormap
 
     def __update_colormap(self):
-        raise NotImplementedError("To be implemented.")
+        with open(r'resources//data.pkl', 'wb') as file:
+            pickle.dump(self.colormap, file)
