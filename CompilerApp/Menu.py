e27965dcfa9c3afc65ab54fb5f0832f5852244b1
LovetheFrogs <102818341+LovetheFrogs@users.noreply.github.com>
2022-10-14 11:34:58 +0200

    Fixed Menu superclass constructor

--- CompilerApp/Menu.py
+++ CompilerApp/Menu.py
@@ -5,7 +5,6 @@ class Menu:
     def __init__(self, window):
         self.menu_bar = tk.Menu(window)
         self.submenus = []
-        self.__generate_tabs
 
     @abstractmethod
     def __generate_tabs(self):
